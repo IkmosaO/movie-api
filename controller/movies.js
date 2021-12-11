0becd4042811d6f6e768b1b708c877986e999887
Ikponmwosa <otasowieik@gmail.com>
2021-12-10 21:18:20 -0500

    added support for all requests asked for by instructions

--- controller/movies.js
+++ controller/movies.js
@@ -7,14 +7,43 @@ const getAllMovies = (request, response) => {
 const getMoviesByTitle = (request, response) => {
   const { title } = request.params
   const findTitle = movies.filter((movie) => {
-    return movie.title.includes(String(title))
+    return movie.title.toLowerCase().includes(String(title))
   })
 
   return response.send(findTitle)
 }
-console.log(movies.filter((movie) => movie.title.includes('Men')))
+
+const getMoviesByDirector = (request, response) => {
+  const { director } = request.params
+  const findDirector = movies.filter((movie) => {
+    return movie.directors.join(' ').toLowerCase().includes(String(director))
+  })
+
+  return response.send(findDirector)
+}
+
+const saveNewMovie = (request, response) => {
+  const {
+    title, directors, releaseDate, rating, runTime, genres
+  } = request.body
+
+  if (!title || !directors || !releaseDate || !rating || !runTime || !genres) {
+    return response.status(400).send('A field is missing')
+  }
+  const newMovie = {
+    title, directors, releaseDate, rating, runTime, genres
+  }
+
+  movies.push(newMovie)
+
+  return response.status(201).send(newMovie)
+}
+
+// console.log(movies.filter((movie) => movie.directors.join(' ').toLowerCase().includes('joseph')))
 
 module.exports = {
   getAllMovies,
-  getMoviesByTitle
+  getMoviesByTitle,
+  getMoviesByDirector,
+  saveNewMovie
 }
--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const movies = require('./movies')
 const app = express()
-const { getAllMovies, getMoviesByTitle } = require('./controller/movies')
+const { getAllMovies, getMoviesByTitle, getMoviesByDirector, saveNewMovie } = require('./controller/movies')
 
 app.get('/movies', getAllMovies)
 
 app.get('/movies/:title', getMoviesByTitle)
 
+app.get('/movies/directors/:director', getMoviesByDirector)
+
+app.post('/movies', bodyParser.json(), saveNewMovie)
+
 app.listen(1337, () => {
-  console.log('listening on port 1331')
+  console.log('listening on port 1337')
 })
